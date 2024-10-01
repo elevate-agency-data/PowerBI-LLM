b1769a987e55dd9883838223e577e18bf60a6be4
Alice Gallais <gallaisalicelfm@gmail.com>
2024-10-01 18:02:56 +0200

    fonction pour comparer schemas jsons

--- build_database.py
+++ build_database.py
@@ -1,22 +1,20 @@
 import json
 import pandas as pd
 
+
 def template_json(instructions, json_path_before_change, json_path_after_change):
 # Fonction pour mettre en forme le prompt avec le json PBI
     with open(json_path_before_change, 'r') as json_data :
-        data_before_change = json.load(json_data)
+        json_before_change = json.load(json_data)
     with open(json_path_after_change, 'r') as json_data :
-        data_after_change = json.load(json_data)
-
-    json_before_change = json.dumps(data_before_change)
-    json_after_change = json.dumps(data_after_change)
+        json_after_change = json.load(json_data)
 
     template = {"messages": [
-        {"role": "system", "content": "You are an experienced Power BI engineer working extensively with Power BI pbix format reports. You are expected to modify the report.json file based on the user description."}, 
-        {"role": "user", "content": f"{instructions} JSON file = {json_before_change}."}, 
-        {"role": "assistant", "content": f"{json_after_change}"}]}
+        {"role": "system", "content": "Tu es un data analyst expert en Power BI qui a l'habitude de travailler avec des rapports Power BI au format pbix. Ton role est de modifier le fichier JSON du rapport fourni selon les instructions fournies par l'utilisateur."}, 
+        {"role": "user", "content": f"{instructions} fichier JSON = {json_before_change}."}, 
+        {"role": "assistant", "content": json_after_change}]}
 
-    final_json = json.dumps(template)
+    final_json = json.dumps(template, indent=4)
 
     return final_json
 
@@ -35,8 +33,9 @@ bdd['json_resultat'] = bdd.apply(lambda row: template_json(row['instructions'], 
 # On ajoute tous les jsons à un fichier jsonl
 with open('bdd_fr.jsonl', 'w') as file:
     for item in bdd['json_resultat']:
-        print(item)
         json_obj = json.loads(item)
-        print(json_obj)
         json.dump(json_obj, file)
         file.write('\n')
+
+
+
